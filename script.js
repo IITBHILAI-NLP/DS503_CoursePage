a3388c53b9a3eb2f872c13655f909a70bd82392e
Anirban Haldar <anirban.haldar600@gmail.com>
2022-10-10 15:42:36 +0530

    All done

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-var a = document.getElementById("material_table");
-var b = document.getElementById("annun");
+function changer(obj) {
+  obj.parentElement.querySelector(".nested").classList.toggle("active");
+  obj.classList.toggle("caret-down");
+}
 
-for (let i = 0; i < materials.length; i++) {
-  let x = materials[i];
-  if (x.length == 1) {
-    let tr = document.createElement("tr");
-    let th = document.createElement("th");
-    th.innerText = x[0];
-    th.setAttribute("colspan", "2");
-    th.classList = ["week_title"];
-    tr.appendChild(th);
-    a.appendChild(tr);
-  }
-  else {
-    let tr = document.createElement("tr");
-    let th1 = document.createElement("th");
-    let th2 = document.createElement("th");
-    let link = document.createElement("a");
-    th1.innerText = x[0];
-    th1.setAttribute("rowspan", x.length - 1);
-    link.innerText = x[1][0];
-    link.setAttribute("href", x[1][1]);
-    link.setAttribute("target", "_blank");
-    tr.appendChild(th1);
-    th2.appendChild(link);
-    tr.appendChild(th2);
-    a.appendChild(tr);
-    for(let j = 2; j < x.length; j++){
-      let trs = document.createElement("tr");
-      let ths = document.createElement("th");
-      let links = document.createElement("a");
-      links.innerText = x[j][0];
-      links.setAttribute("href", x[j][1]);
-      links.setAttribute("target", "_blank");
-      ths.appendChild(links);
-      trs.appendChild(ths);
-      a.appendChild(trs);
-    }
+function runner(path) {
+  if (path.includes(".")) {
+    // File
+    var elem = document.createElement("li");
+    elem.innerText = path.split("/").pop();
+    return elem;
+  } else {
+    // Folder
+    var fold = document.createElement("li");
+    var value = document.createElement("span");
+    value.classList = ["caret"];
+    value.innerText = path.split("/").pop();
+    value.setAttribute("onclick", "changer(this)");
+    fold.appendChild(value);
+    var child = document.createElement("ul");
+    child.classList = ["nested"];
+    $.getJSON(path, (data) => {
+      data.forEach((x) => {
+        child.appendChild(runner(path + "/" + x));
+      });
+    });
+    fold.appendChild(child);
+    return fold;
   }
 }
+
+document.getElementById("my_treeview").appendChild(runner("Course_material"));
+
+var b = document.getElementById("annun");
 
 for (let i = announcements.length - 1; i >= 0; i--) {
   let x = announcements[i];
